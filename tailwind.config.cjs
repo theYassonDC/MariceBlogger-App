75983dfa510885980d0ff6344fd10cb4c65fb6c0
YassonDC <yassonword@gmail.com>
2022-11-08 03:46:32 -0500

    aa

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./src/**/*.{html,js,jsx}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
